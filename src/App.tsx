5a66d55e225d497b6575cddefe654b41925454b3
Omri Ruvio <omriruvio@gmail.com>
2023-10-01 21:04:03 +0300

    initial polygon drawing and logging

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,79 @@ import { useState, useRef, useCallback, useEffect } from "react";
 
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-function MyMapComponent({
-  center,
-  zoom,
-}: {
+interface MyMapProps {
   center: google.maps.LatLngLiteral;
   zoom: number;
-}) {
-  const ref = useRef();
-  const [map, setMap] = useState<google.maps.Map>(null);
+}
 
-  
-  useEffect(() => {
-    const map = new window.google.maps.Map(ref.current, {
-      center,
-      zoom,
-    });
-    setMap(map);
-  }, []);
+const MyMapComponent: React.FC<MyMapProps> = ({ center, zoom }) => {
+  const ref = useRef(null);
+  const mapRef = useRef<google.maps.Map>();
+  const [polygon, setPolygon] = useState<google.maps.Polygon>(null);
+  const polygonListeners = useRef<google.maps.MapsEventListener[]>([]);
+
+  const [polygonCoords, setPolygonCoords] = useState<google.maps.LatLngLiteral[]>([
+    { lat: 52.52549080781086, lng: 13.398118538856465 },
+    { lat: 52.48578559055679, lng: 13.36653284549709 },
+    { lat: 52.48871246221608, lng: 13.44618372440334 }
+  ]);
+
+  const updatePolygonCoords = () => {
+    if (polygon) {
+      const pathArray = polygon.getPath().getArray();
+      setPolygonCoords(
+        pathArray.map(point => {
+          return { lat: point.lat(), lng: point.lng() };
+        })
+      );
+    }
+  };
 
   useEffect(() => {
-    const polygon = new window.google.maps.Polygon({
-      paths: [
-        { lat: 52.52549080781086, lng: 13.398118538856465 },
-        { lat: 52.48578559055679, lng: 13.36653284549709 },
-        { lat: 52.48871246221608, lng: 13.44618372440334 }
-      ],
+    if (!mapRef.current) {
+      mapRef.current = new window.google.maps.Map(ref.current, {
+        center,
+        zoom
+      });
+    }
+    
+    const newPolygon = new window.google.maps.Polygon({
+      paths: polygonCoords,
+      editable: true,
+      draggable: true,
       strokeColor: "#FF0000",
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: "#FF0000",
       fillOpacity: 0.35
     });
-  
-    polygon.setMap(map);
-  }, [map]);
+    newPolygon.setMap(mapRef.current);
+    setPolygon(newPolygon);
 
-  return <div ref={ref} id="map" style={{width: "100%", height: "100%"}} />;
-}
+    // Update polygon coordinates when edited
+    polygonListeners.current.push(
+      newPolygon.getPath().addListener('set_at', updatePolygonCoords),
+      newPolygon.getPath().addListener('insert_at', updatePolygonCoords),
+      newPolygon.getPath().addListener('remove_at', updatePolygonCoords)
+    );
+
+    // Log polygon coordinates when clicked
+    newPolygon.addListener('click', () => {
+      console.log(polygonCoords);
+    });
+
+    // Cleanup
+    return () => {
+      polygonListeners.current.forEach(listener => listener.remove());
+    };
+  }, []);
+
+  return (
+    <div ref={ref} id="map" style={{ width: "100%", height: "100%" }}>
+      <button onClick={() => console.log(polygonCoords)}>Done</button>
+    </div>
+  );
+};
 
 const render = (status) => {
   switch (status) {
